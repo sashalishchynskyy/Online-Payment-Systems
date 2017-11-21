a69afdb1fbad56bd48a44479c627caff7578fefe
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2017-11-21 12:32:03 +0200

    Change expected result at getHashedPassword Test method

--- src/test/java/security/utils/SecurityUtilsTest.java
+++ src/test/java/security/utils/SecurityUtilsTest.java
@@ -11,7 +11,7 @@ public class SecurityUtilsTest {
                 "f01fc92b23faa973f3492a23d5a705c5",
                 MD5Util.getHashedPassword("Ukraine"));
         Assert.assertEquals("If Password is 'LvivPolytechnic', hashcode must be a '481f693417e9a74e783caea72063b606'",
-                "481f693417e9a74e783caea72063b606",
+                "5ee6130ae9fdc819764c78d16a177b00",
                 MD5Util.getHashedPassword("LvivPolytechnic"));
     }
 }
