49f0d5ea55f2002dcbbe4af07d63eef8b27c2999
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2018-02-01 23:18:47 +0200

    Delete trash from Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,10 @@
 package main;
 
-import security.utils.MD5Util;
+import menu.StartMenu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MD5Util.getHashedPassword(MD5Util.getStringForPassword()));
+        StartMenu startMenu = new StartMenu();
+        startMenu.start();
     }
 }
