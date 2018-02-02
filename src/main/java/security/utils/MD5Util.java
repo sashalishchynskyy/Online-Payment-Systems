450acc838c97b3ea9ce6433ae0c63d3c78533d1d
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2018-02-02 14:49:44 +0200

    Remove getStringForPassword() method on MD5Util class

--- src/main/java/security/utils/MD5Util.java
+++ src/main/java/security/utils/MD5Util.java
@@ -9,23 +9,12 @@ import java.security.NoSuchAlgorithmException;
 public class MD5Util {
     private static final Logger logger = Logger.getLogger(MD5Util.class);
 
-            logger.error("Unexpected error", e);
     public static String hashedString(String string) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance("MD5");
         messageDigest.reset();
         messageDigest.update(string.getBytes());
         byte[] digest = messageDigest.digest();
         BigInteger bigInt = new BigInteger(1, digest);
-    public static String getStringForPassword() {
-        String passwordString;
-        while (true) {
-            passwordString = ScannerTool.getString();
-            if (passwordString.matches("\\w{5,15}")) {
-                logger.info("User input right data!");
-                return passwordString;
-            }
-            System.out.println("Wrong input!");
-            logger.warn("User input wrong data!");
-        }
+        return bigInt.toString(16);
     }
 }
