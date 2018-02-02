8b17c3824558490aec9eed875ea47e7b9f5bd2b6
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2018-02-02 14:03:31 +0200

    Change testHashedString() method on SecurityUtilsTest class

--- src/test/java/security/utils/SecurityUtilsTest.java
+++ src/test/java/security/utils/SecurityUtilsTest.java
@@ -1,17 +1,17 @@
 package security.utils;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.security.NoSuchAlgorithmException;
 
 public class SecurityUtilsTest {
     @Test
-    public void testGetHashedPassword() {
-        MD5Util MD5Util = new MD5Util();
-        Assert.assertEquals("If Password is 'Ukraine', hashcode must be a 'f01fc92b23faa973f3492a23d5a705c5'",
+    public void testHashedString() throws NoSuchAlgorithmException {
+        assertEquals("If Password is 'Ukraine', hashcode must be a 'f01fc92b23faa973f3492a23d5a705c5'",
                 "f01fc92b23faa973f3492a23d5a705c5",
-                MD5Util.getHashedPassword("Ukraine"));
-        Assert.assertEquals("If Password is 'LvivPolytechnic', hashcode must be a '481f693417e9a74e783caea72063b606'",
+                MD5Util.hashedString("Ukraine"));
+        assertEquals("If Password is 'LvivPolytechnic', hashcode must be a '481f693417e9a74e783caea72063b606'",
                 "5ee6130ae9fdc819764c78d16a177b00",
-                MD5Util.getHashedPassword("LvivPolytechnic"));
+                MD5Util.hashedString("LvivPolytechnic"));
     }
 }
