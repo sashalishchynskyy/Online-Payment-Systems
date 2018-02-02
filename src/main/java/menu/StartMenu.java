5ea6a0242cc23395df61d0d9dd6f35b25f937958
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2018-02-02 15:04:27 +0200

    Create StartMenu class

---
+++ src/main/java/menu/StartMenu.java
@@ -0,0 +1,34 @@
+package menu;
+
+import utils.scanner.StartMenuScanner;
+import view.StartMenuView;
+
+public class StartMenu {
+    private SignInMenu signInMenu;
+    private SignUpMenu signUpMenu;
+
+    public void start() {
+        StartMenuView.printStartMenu();
+        chooseMenuOption();
+    }
+
+    private void chooseMenuOption() {
+        System.out.println();
+        switch (StartMenuScanner.getStartMenuOption()) {
+            case 1:
+                signInMenu = SignInMenu.getInstance();
+                signInMenu.start();
+                break;
+            case 2:
+                signUpMenu = SignUpMenu.getInstance();
+                signUpMenu.start();
+                break;
+            case 3:
+                exit();
+        }
+    }
+
+    private void exit() {
+        StartMenuView.printExit();
+    }
+}
