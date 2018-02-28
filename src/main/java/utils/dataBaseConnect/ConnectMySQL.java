f13e83f8da5fbe38928fdabc21b9ef76f4b99a35
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2018-02-28 20:48:47 +0200

    Create loadJDBCDriver method for loading the JDBC Driver

--- src/main/java/utils/dataBaseConnect/ConnectMySQL.java
+++ src/main/java/utils/dataBaseConnect/ConnectMySQL.java
@@ -10,4 +10,58 @@ public class ConnectMySQL {
     private static final String CONN_STRING =
             "jdbc:mysql://127.0.0.1:3306/users_data";
 
+    public static void main(String[] args) throws SQLException {
+        openConnection();
+        createStatement();
+        ResultSet resultSet = null;
+        try {
+            String sql = "select * from usernames";
+            resultSet = getResultSet(sql);
+            while (resultSet.next()) {
+                String name = resultSet.getString("username");
+                System.out.println("Rows:" + name);
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            if (resultSet != null) resultSet.close();
+            closeConnection();
+        }
+    }
+
+    public static ResultSet getResultSet(String query) {
+        try {
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void openConnection() {
+        try {
+            connection = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void closeConnection() {
+        try {
+            if (connection != null) connection.close();
+            if (statement != null) statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createStatement() {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
