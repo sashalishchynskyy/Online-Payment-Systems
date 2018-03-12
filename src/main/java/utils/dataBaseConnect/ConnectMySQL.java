9df09bc871f83dd802228295a97721bde16bea44
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2018-03-12 19:45:39 +0200

    Change password for connection to Database

--- src/main/java/utils/dataBaseConnect/ConnectMySQL.java
+++ src/main/java/utils/dataBaseConnect/ConnectMySQL.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class ConnectMySQL {
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "root_password";
     private static final String CONN_STRING =
             "jdbc:mysql://127.0.0.1:3306/users_data";
 
